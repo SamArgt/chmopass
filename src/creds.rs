9a7113de783bf140dd0188345414dd0787a94b03
SamArgt <36817478+SamArgt@users.noreply.github.com>
2025-03-15 01:06:37 +0100

    add clone

--- src/creds.rs
+++ src/creds.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
 
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ServiceCredentials {
     pub client_id: String,
     pub client_secret: String,
